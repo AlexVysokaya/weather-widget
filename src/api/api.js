02869effaef2001f84bfdb4e42f14968179818c6
AlexVysokaya <alieksasha@bk.ru>
2021-04-08 20:44:49 +0300

    3

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const API_KEY = '4fa52b791cd0228644eea9194115d5df';
 
 export const widgetAPI = {
   getData(city) {
-    return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${API_KEY}`)
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${API_KEY}`)
       .then(response => response.data)
   }
 }
